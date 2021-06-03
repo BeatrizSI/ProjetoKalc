70f0ff17905db6afae657d17e92332681e0c4764
Beatriz Magalhães <bia.netadm2015@gmail.com>
2021-06-03 16:10:22 -0300

    Projeto - PWEB2

---
+++ js/kalcWeb.js
@@ -0,0 +1,81 @@
+console.log("Teste Calculadora");
+
+//DOM
+
+// document.write("Boa tarde");
+
+//Operações 
+const igual = document.getElementById("igual");
+const divisao = document.getElementById("divisao");
+const multiplicacao = document.getElementById("multiplicacao");
+const subtracao = document.getElementById("subtracao");
+const adicao = document.getElementById("adicao");
+
+// Numeros
+const quatro = document.getElementById("quatro");
+
+console.log(igual);
+console.log(quatro);
+console.log(nove);
+
+//Eventos Operações
+ igual.onclick = function() {
+    console.log(igual.innerText);
+ };
+
+ divisao.onclick = function() {
+    console.log(divisao.innerText);
+ };
+
+ multiplicacao.onclick = function() {
+    console.log(multiplicacao.innerText);
+ };
+
+ subtracao.onclick = function() {
+    console.log(subtracao.innerText);
+ };
+
+ adicao.onclick = function() {
+    console.log(adicao.innerText);
+ };
+ 
+ 
+ //Eventos Números
+ nove.onclick = function(){
+    console.log(nove.innerText);
+ };
+ oito.onclick = function(){
+    console.log(oito.innerText);
+ };
+ sete.onclick = function(){
+    console.log(sete.innerText);
+ };
+ seis.onclick = function(){
+    console.log(seis.innerText);
+ };
+ cinco.onclick = function(){
+    console.log(cinco.innerText);
+ };
+ quatro.onclick = function(){
+    console.log(quatro.innerText);
+ };
+ tres.onclick = function(){
+    console.log(tres.innerText);
+ };
+ dois.onclick = function(){
+    console.log(dois.innerText);
+ };
+ um.onclick = function(){
+    console.log(um.innerText);
+ };
+ zero.onclick = function(){
+    console.log(zero.innerText);
+ };
+
+ ponto.onclick = function(){
+    console.log(ponto.innerText);
+ };
+
+ clear.onclick = function(){
+    console.log(clear.innerText);
+ };
